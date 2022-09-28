af12c8e9af7e419a9f2b76c015183a5e5794d39a
arishuang10 <aris.huang10@gmail.com>
2022-09-28 00:26:59 -0500

    Added Billboarding Effect
    All the feature markers will now face the user wherever he or she goes. It's working correctly. Also, the distance text hierarchy has been further tested and working correctly as well. One caveat, though, is that somehow we are not locally instantiating the features listed from the server. As of now, we are only instantiating markers that we placed locally. This might be something that we would have to look into further.

--- Assets/Scripts/BillboardingFeatureMarker.cs
+++ Assets/Scripts/BillboardingFeatureMarker.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class BillboardingFeatureMarker : MonoBehaviour
 {
     public GameObject cam;
+    public GameObject marker_parent;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,7 +15,12 @@ public class BillboardingFeatureMarker : MonoBehaviour
     // Update is called once per frame
     void LateUpdate()
     {
-        transform.LookAt(cam.transform);
-        transform.rotation = Quaternion.Euler(0f, transform.rotation.eulerAngles.y, 0f);
+        foreach (Transform child in marker_parent.transform)
+        {
+            child.transform.LookAt(cam.transform);
+            child.rotation = Quaternion.Euler(0f, child.transform.rotation.eulerAngles.y, 0f);
+        }
+        //transform.LookAt(cam.transform);
+        //transform.rotation = Quaternion.Euler(0f, transform.rotation.eulerAngles.y, 0f);
     }
 }
