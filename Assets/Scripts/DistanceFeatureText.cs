aba5023865778f2b97f04d55baea7b07998bc6c1
arishuang10 <aris.huang10@gmail.com>
2022-10-11 16:23:34 -0500

    Working on coroutine

--- Assets/Scripts/DistanceFeatureText.cs
+++ Assets/Scripts/DistanceFeatureText.cs
@@ -21,6 +21,8 @@ public class DistanceFeatureText : MonoBehaviour
     {
         cam = GameObject.Find("Main Camera");
         isFeet = true;
+        cam_pos = cam.GetComponent<Transform>().position;
+        oldPos = cam_pos;
         //StartCoroutine("HeadsetDistanceCalculate");
     }
 
