a81105b2631ec2eb2383b9ec8e728688be8ac7c4
arishuang10 <aris.huang10@gmail.com>
2023-02-01 23:00:57 -0600

    Navigation Update
    Still working on the Navigation.cs script, I added a tag ('Icon') to each of the map icons for detecting if the icon is being pressed on the palm map. However, for some reason gameobject detection is not working (perhaps this functionality is disabled?). I also added a line renderer attached to the main camera. This is to draw a line between the user and the desire landmark/icon, but this is not working so far as well.

--- Assets/Scripts/Navigation.cs
+++ Assets/Scripts/Navigation.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class Navigation : MonoBehaviour
 {
     GameObject markerSpawnParent = null;
+    Collider collider;
+    LineRenderer line;
+    [SerializeField] Transform[] points; //this contains the position of the landmark/icon
     // Start is called before the first frame update
     void Start()
     {
@@ -13,12 +16,24 @@ public class Navigation : MonoBehaviour
         {
             Debug.Log("Navigation: cannot find the icon parent");
         }
+        collider = this.GetComponent<Collider>();
+        //set the collider active --> TODO: might not need this anymore
+        collider.enabled = true;
+        // initializing the line render
+        line = GameObject.Find("Main Camera").GetComponent<LineRenderer>();
+        if (line != null) { Debug.Log("found line renderer!"); }
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.touchCount > 0 && Input.touches[0].phase == TouchPhase.Began)
+        Debug.Log("Got to Update() from Navigation");
+        //update the position of user and landmark 
+        points[0] = Camera.main.transform;
+        points[1] = markerSpawnParent.transform.Find(this.name); // might move this line to elsewhere, but for now, it should work fine
+
+        // TODO: ask why this is not happening... is the interaction not enabled...? Need to ask Bryce about the map icon prefab
+        if (Input.touchCount > 0 && (Input.GetTouch(0).phase == TouchPhase.Began))
         {
             Debug.Log("Got to the Navigation Touch Update");
             Ray detectRay = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
@@ -39,5 +54,8 @@ public class Navigation : MonoBehaviour
     {
         //TODO: this is where I should add the line render between the user and the desire landmark/icon
         Debug.Log("Touched the icon!");
+        line.SetPosition(0, points[0].position); // the position of the user (i.e. the main camera's position)
+        line.SetPosition(1, points[1].position); // the position of the desire landmark/icon
+        // Note: I set the line renderer to be in world space, so it may not render because of this 
     }
 }
