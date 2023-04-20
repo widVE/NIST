a69002d7ddd74e3d15d3ab0e078039b136d39a7f
Pallasite <Pallasite@users.noreply.github.com>
2023-04-20 17:24:48 -0500

    Upgraded the rotation script, only y rotaiton
    Now we can control which rotation components we copy from the source object. In this case we only want the compass orientation, ie y axis rotation, to copy over. The other axis will cause the map icon to clip into the map.
    TODO: Only rotate the local headset icon based on the camera value and not all of the icons

--- Assets/OffsetRotation.cs
+++ Assets/OffsetRotation.cs
@@ -6,13 +6,23 @@ public class OffsetRotation : MonoBehaviour
 {
     public GameObject source_transform;
 
+    //Editor facing control variables
     public bool find_main_camera = true;
+    public bool copy_x_rotation = false;
+    public bool copy_y_rotation = true;
+    public bool copy_z_rotation = false;
     public string rotation_camera_name = "Main Camera";
 
     // Update is called once per frame
     void Update()
     {
-        this.transform.localEulerAngles = source_transform.transform.eulerAngles;
+        Vector3 euler_rotation_source = new Vector3();
+
+        if(copy_x_rotation) euler_rotation_source.x = source_transform.transform.eulerAngles.x;
+        if(copy_y_rotation) euler_rotation_source.y = source_transform.transform.eulerAngles.y;
+        if(copy_z_rotation) euler_rotation_source.z = source_transform.transform.eulerAngles.z;
+
+        this.transform.localEulerAngles = euler_rotation_source;
     }
 
     private void Start()
