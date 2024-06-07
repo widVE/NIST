2942e0388a619c478e9fd97343fc2fc5c878b628
Bryce <Pallasite@users.noreply.github.com>
2024-06-06 20:13:27 -0500

    Activation and deactivation of componenets
    I've set up the componenets to properly activate and deactivate based on the adjustement state of the map. The box colliders need to be toggled between the two grabbale objects because if one collier is inside the other only the outer one is activated. I've also disabled the mesh renderer for the bounding box when it's not active.

--- Assets/SelectiveEnabler.cs
+++ Assets/SelectiveEnabler.cs
@@ -6,6 +6,8 @@ public class SelectiveEnabler : MonoBehaviour
 {
     //list of  activeable objects
     public GameObject[] managed_objects;
+    public GameObject[] managed_colliders;
+    public GameObject[] managed_renderers;
     
     // Start is called before the first frame update
     void Start()
@@ -31,6 +33,51 @@ public class SelectiveEnabler : MonoBehaviour
             else
             {
                 game_object.SetActive(false);
+            }
+        }
+    }
+
+
+    // same as EnableObject, but this time it only affects the Unity BoxCollider component
+    public void EnableObjectCollider(GameObject activated_object)
+    {
+        foreach (GameObject game_object in managed_colliders)
+        {
+            if (game_object == activated_object)
+            {
+                game_object.GetComponent<BoxCollider>().enabled = true;
+            }
+            else
+            {
+                game_object.GetComponent<BoxCollider>().enabled = false;
+            }
+        }
+    }
+
+    // same as EnableObject, but this time it only affects the Unity MeshRenderer component
+    public void EnableObjectRenderer(GameObject activated_object)
+    {
+        foreach (GameObject game_object in managed_renderers)
+        {
+            if (game_object == activated_object)
+            {
+                game_object.GetComponent<MeshRenderer>().enabled = true;
+            }
+            else
+            {
+                game_object.GetComponent<MeshRenderer>().enabled = false;
+            }
+        }
+    }
+    
+    // same as rendered but turns it off
+    public void DisableObjectRenderer(GameObject activated_object)
+    {
+        foreach (GameObject game_object in managed_renderers)
+        {
+            if (game_object == activated_object)
+            {
+                game_object.GetComponent<MeshRenderer>().enabled = false;
             }
         }
     }
