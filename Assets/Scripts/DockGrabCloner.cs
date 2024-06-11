3b34e2fbf4bad27bd46d442f32e81fd8864be48d
Bryce <Pallasite@users.noreply.github.com>
2024-06-11 18:22:55 -0500

    Updated the spawning and movement
    Now the bottom of the map spawns where the globe is placed, making it easier to adjust the buttons. Unfortunately the button placement code is kinda broken. in the MRTK implementation the y offset isn't used. i was able to make a fix, but the script is stored in the packages part of Unity so it's not synced via github. I'm not quite sure what to do with that. We could make our own AppBar.cs copy, but I'm not sure what the consequcnces will be. I wasn't able to figure out how to make the box immedeatly editable yet, it still needs the button press at the moment. I also am seeing some weird distance interaction behavior where the cursor collides with a box collider that shouldn't be there anymore.

--- Assets/Scripts/DockGrabCloner.cs
+++ Assets/Scripts/DockGrabCloner.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using Unity.Mathematics;
 using Unity.Sentis.Layers;
 using UnityEngine;
+using System.Linq;
 
 public class DockGrabCloner : MonoBehaviour
 {
@@ -15,6 +16,9 @@ public class DockGrabCloner : MonoBehaviour
     //volumetric map prefab that will be spawned at the location the user lets go of the docked object at
     public GameObject volumetric_map_prefab;
 
+    //Name of the culling region in the volumetric map prefab, this is the ultimate size of the object
+    public string culling_box_name = "Manual Culling Box Adjustment";
+
     //funtion, when this object is picked up it will spawn a prefab at the spawn_parent_location
     public void SpawnObject(GameObject prefab)
     {
@@ -25,8 +29,17 @@ public class DockGrabCloner : MonoBehaviour
     //spawned at the location the user lets go of the docked object at, has no parent   
     public void SpawnVolumetricMap(GameObject prefab)
     {
+        //find the child of volumetric_map_prefab GameObject that has the name "VolumeMap"
+        //this is using LINQ, which is a way to query objects in a collection. I'm not really sure how it works, but I thought it was cool
+        GameObject culling_region = volumetric_map_prefab.GetComponentsInChildren<Transform>().FirstOrDefault(c => c.gameObject.name == culling_box_name)?.gameObject;
+
+        //get the y extents bounds of the volume map and add that value to spawn_position.y
+        //so the volume map is spawned above the object
+        Vector3 spawn_position = new Vector3(this.transform.position.x, this.transform.position.y, this.transform.position.z);  
         
-        GameObject volume_map = Instantiate(prefab, this.transform.position, quaternion.identity);
+        spawn_position.y += culling_region.GetComponent<BoxCollider>().bounds.extents.y;
+        
+        GameObject volume_map = Instantiate(prefab, spawn_position, quaternion.identity);
     }
 
     // Destroy this object
