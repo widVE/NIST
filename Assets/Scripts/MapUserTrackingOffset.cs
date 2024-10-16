2b2251c8c7fb9990b7793066193f50d1b0632cf0
Lance Hartung <lhartung@wisc.edu>
2024-10-16 13:58:29 -0500

    Fix the map offset calculation for correct clipping

--- Assets/Scripts/MapUserTrackingOffset.cs
+++ Assets/Scripts/MapUserTrackingOffset.cs
@@ -9,9 +9,6 @@ public class MapUserTrackingOffset : MonoBehaviour
     public GameObject main_camera;
     public GameObject map_frame_offset;
 
-    private Vector3 centering_offset;
-
-
     // Start is called before the first frame update
     void Start()
     {
@@ -19,26 +16,10 @@ public class MapUserTrackingOffset : MonoBehaviour
         if (!main_camera) main_camera = GameObject.Find("Main Camera");               
     }
 
-    public void DelayedStart()
-    {
-        Invoke("SetOffsetStart", 1f);
-    }
-
     // Update is called once per frame
     void Update()
     {
-        //the map frame offset is the difference between the starting offset and the main camera
-        map_frame_offset.transform.localPosition = centering_offset - new Vector3(main_camera.transform.position.x, main_camera.transform.position.z, 0);
-
-
+        var offset = new Vector3(-main_camera.transform.position.x, -main_camera.transform.position.z, 0) - starting_offset.transform.localPosition;
+        map_frame_offset.transform.localPosition = offset;
     }
-
-    void SetOffsetStart()
-    {
-        centering_offset = Vector3.zero - starting_offset.transform.localPosition;
-
-        map_frame_offset.transform.localPosition = centering_offset;
-    }
-
-
 }
